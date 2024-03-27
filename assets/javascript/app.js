763975b9b466d693c745a48346c0c5dfe9b43d8e
ANS <36783010+answebdev@users.noreply.github.com>
2024-03-27 08:57:12 -0700

    remove comment section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,24 +31,23 @@ yearSpan.forEach((yearText) => {
 /* - - - CONFIGURATION VARIABLES - - - */
 
 // make sure the id is yours
-window.gc_params = {
-  graphcomment_id: 'ESLToolbox',
+// window.gc_params = {
+//   graphcomment_id: 'ESLToolbox',
 
-  // if your website has a fixed header, indicate it's height in pixels
-  fixed_header_height: 0,
-};
+//   fixed_header_height: 0,
+// };
 
 /* - - - DON'T EDIT BELOW THIS LINE - - - */
 
-(function () {
-  var gc = document.createElement('script');
-  gc.type = 'text/javascript';
-  gc.async = true;
-  gc.src =
-    'https://graphcomment.com/js/integration.js?' +
-    Math.round(Math.random() * 1e8);
-  (
-    document.getElementsByTagName('head')[0] ||
-    document.getElementsByTagName('body')[0]
-  ).appendChild(gc);
-})();
+// (function () {
+//   var gc = document.createElement('script');
+//   gc.type = 'text/javascript';
+//   gc.async = true;
+//   gc.src =
+//     'https://graphcomment.com/js/integration.js?' +
+//     Math.round(Math.random() * 1e8);
+//   (
+//     document.getElementsByTagName('head')[0] ||
+//     document.getElementsByTagName('body')[0]
+//   ).appendChild(gc);
+// })();
