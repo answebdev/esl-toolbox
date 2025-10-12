bcb679c2d31cad2b8acb0c1007e9d5bc5a5c2ff3
Adolf Schmuck <britvaica@netscape.net>
2025-10-12 07:11:24 -0700

    update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,17 @@
 // cypress.config.js
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Ensures Cypress finds your tests
     setupNodeEvents(on, config) {
       // Register the custom task
       on('task', {
         logToTerminal(message) {
-          console.log(message); // Log the message to the terminal
-          return null; // Return null because tasks must return a value
+          console.log(message);
+          return null;
         },
       });
     },
   },
-};
+});
